87aff6d70087658bbeb60972d088c3804d5f2c7a
bitfws <bitfws.xai@gmail.com>
2026-01-28 22:54:57 -0500

    fixed xyz

--- controllers/invController.js
+++ controllers/invController.js
@@ -9,9 +9,17 @@ const invCont = {};
 invCont.buildByClassificationId = async function (req, res, next) {
   const classification_id = req.params.classificationId;
   const data = await invModel.getInventoryByClassificationId(classification_id);
+
+  if (!data || data.length === 0) {
+    const err = new Error('No vehicles found for this classification.');
+    err.status = 404;
+    return next(err);
+  }
+
   const grid = await utilities.buildClassificationGrid(data);
   let nav = await utilities.getNav();
   const className = data[0].classification_name;
+
   res.render('./inventory/classification', {
     title: className + ' vehicles',
     nav,
